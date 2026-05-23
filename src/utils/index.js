2a1ce21f6ec1e40916a957f16828aada859b56c1
Thirawut <tthirawut06@gmail.com>
2026-05-23 17:00:17 +0700

    feat: refactor PageNotFound component; move to pages directory and remove unused files

--- src/utils/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Convert a page name to a URL-friendly slug.
- * @param {string} pageName - The page name to convert
- * @returns {string} URL-friendly slug starting with /
- */
-export function createPageUrl(pageName) {
-  return '/' + pageName.replace(/ /g, '-');
-}
